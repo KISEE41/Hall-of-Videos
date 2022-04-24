727ea3ec97c732ea361b76b0b112b02b8c5d4f6b
KISEE41 <shreejan.kisee005@gmail.com>
2022-04-24 14:09:58 +0545

    First commit

---
+++ halls/forms.py
@@ -0,0 +1,17 @@
+from pyexpat import model
+from django import forms
+
+from .models import Video
+
+
+class VideoForm(forms.ModelForm):
+    class Meta:
+        model = Video
+        fields = ['url']
+        labels = {
+            'url': 'YouTube Url',
+        }
+
+
+class SearchForm(forms.Form):
+    search_term = forms.CharField(max_length=150, label='Search for Videos:')
